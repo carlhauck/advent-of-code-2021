04f609344dd255785f3d464e127a2d62a0288803
carlhauck <carlhauck@gmail.com>
2021-12-11 02:08:55 -0600

    adds day 11 solution

---
+++ day-11-dumbo-octopus2.rb
@@ -0,0 +1,49 @@
+class DumboOctopus
+  def initialize(puzzle_input)
+    @line_size = puzzle_input.first.length
+    @num_array = puzzle_input.join("").split("").map(&:to_i)
+  end
+
+  def call
+    index = 0
+    while true
+      @flashed_cells = {}
+      @num_array = increase_energy_level
+      @lines = @num_array.each_slice(@line_size).to_a
+      @lines.each_with_index do |line, y|
+        line.each_with_index do |_, x|
+          flash(x, y) if @lines[y][x] > 9 && !@flashed_cells[[x, y]]
+        end
+      end
+      index += 1
+      @num_array = reset_energy
+      return index if @num_array.uniq.length == 1
+    end
+  end
+end
+
+def increase_energy_level
+  @num_array.map { |num| num += 1 }
+end
+
+def flash(x, y)
+  @lines[y][x] += 1
+  if @lines[y][x] > 9 && !@flashed_cells[[x, y]]
+    @flashed_cells[[x, y]] = true
+    flash(x - 1, y - 1) unless y == 0 || x == 0 # top left
+    flash(x, y - 1) unless y == 0 # top
+    flash(x + 1, y - 1) unless y == 0 || x == @line_size - 1 # top right
+    flash(x - 1, y) unless x == 0 # left
+    flash(x + 1, y) unless x == @line_size - 1 # right
+    flash(x - 1, y + 1) unless y == @lines.length - 1 || x == 0 # bottom left
+    flash(x, y + 1) unless y == @lines.length - 1 # bottom
+    flash(x + 1, y + 1) unless y == @lines.length - 1 || x == @line_size - 1 # bottom right
+  end
+end
+
+def reset_energy
+  @lines.flatten.map { |num| num > 9 ? 0 : num }
+end
+
+puzzle_input = File.open("puzzle_inputs/day-11.txt").readlines.map(&:chomp)
+p DumboOctopus.new(puzzle_input).call
